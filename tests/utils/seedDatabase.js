6949844e12a8ca22e5e3c78e9194aa750a4c5909
Sergio <38168163+FundacionBlockchainChile@users.noreply.github.com>
2021-03-05 15:53:31 -0300

    Setup book project

--- tests/utils/seedDatabase.js
+++ tests/utils/seedDatabase.js
@@ -1,42 +1,44 @@
-import bcrypt from 'bcryptjs'
-import jwt from 'jsonwebtoken'
-import prisma from '../../src/prisma'
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import prisma from '../../src/prisma';
 
 const userOne = {
-    input: {
-        name: 'Jen',
-        email: 'jen@example.com',
-        password: bcrypt.hashSync('Red098!@#$')
-    },
-    user: undefined,
-    jwt: undefined
-}
+  input: {
+    name: 'Jen',
+    email: 'jen@example.com',
+    password: bcrypt.hashSync('Red098!@#$'),
+  },
+  user: undefined,
+  jwt: undefined,
+};
 
 const userTwo = {
-    input: {
-        name: 'Jeff',
-        email: 'jeff@example.com',
-        password: bcrypt.hashSync('PassForJeff')
-    },
-    user: undefined,
-    jwt: undefined
-}
+  input: {
+    name: 'Jeff',
+    email: 'jeff@example.com',
+    password: bcrypt.hashSync('PassForJeff'),
+  },
+  user: undefined,
+  jwt: undefined,
+};
 
 const seedDatabase = async () => {
-    // Delete test data
-    await prisma.mutation.deleteManyUsers()
+  jest.setTimeout(30000);
 
-    // Create user one
-    userOne.user = await prisma.mutation.createUser({
-        data: userOne.input
-    })
-    userOne.jwt = jwt.sign({ userId: userOne.user.id }, process.env.JWT_SECRET)
+  // Delete test data
+  await prisma.mutation.deleteManyUsers();
 
-    // Create user two
-    userTwo.user = await prisma.mutation.createUser({
-        data: userTwo.input
-    })
-    userTwo.jwt = jwt.sign({ userId: userTwo.user.id }, process.env.JWT_SECRET)
-}
+  // Create user one
+  userOne.user = await prisma.mutation.createUser({
+    data: userOne.input,
+  });
+  userOne.jwt = jwt.sign({ userId: userOne.user.id }, process.env.JWT_SECRET);
 
-export { seedDatabase as default, userOne, userTwo }
+  // Create user two
+  userTwo.user = await prisma.mutation.createUser({
+    data: userTwo.input,
+  });
+  userTwo.jwt = jwt.sign({ userId: userTwo.user.id }, process.env.JWT_SECRET);
+};
+
+export { seedDatabase as default, userOne, userTwo };
